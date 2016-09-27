47952cbc8dc7b3b28a6c12e1e2988511b65c32cc
Richard Olsson <r@richardolsson.se>
2016-09-27 14:20:41 +0200

    Add error handling and finished step to registration

--- src/components/pages/landing/SignUpSplash.jsx
+++ src/components/pages/landing/SignUpSplash.jsx
@@ -1,24 +1,65 @@
+import { connect } from 'react-redux';
 import cx from 'classnames';
 import { FormattedMessage as Msg } from 'react-intl';
 import React from 'react';
 import { injectIntl } from 'react-intl';
 import { Link } from 'react-router';
 
+import { register } from '../../../actions/register';
 
+
+const mapStateToProps = state => ({
+    register: state.get('register'),
+});
+
+@connect(mapStateToProps)
 @injectIntl
 export default class SignUpSplash extends React.Component {
+    static propTypes = {
+        showForm: React.PropTypes.bool,
+        onSignUp: React.PropTypes.func,
+    };
+
     render() {
+        let store = this.props.register;
         let content;
 
-        if (this.props.showForm) {
-            let msg = id => this.props.intl.formatMessage({
-                id: 'pages.landing.splash.form.' + id });
+        if (this.props.showForm && store.get('isComplete')) {
+            let values = store.get('data').toJS();
+
+            content = (
+                <div className="SignUpSplash-done">
+                    <Msg tagName="h1" id="pages.landing.splash.done.h1"
+                        values={ values }/>
+                    <Msg tagName="h2" id="pages.landing.splash.done.h2"
+                        values={ values }/>
+                </div>
+            );
+        }
+        else if (this.props.showForm) {
+            let msg = (id, values) => this.props.intl.formatMessage({
+                id: 'pages.landing.splash.form.' + id }, values);
+
+            let error = store.get('error');
+            let errorMessage;
+            if (error) {
+                if (error.httpStatus == 409) {
+                    let values = store.get('data').toJS();
+
+                    errorMessage = (
+                        <Msg className="SignUpSplash-error"
+                            id="pages.landing.splash.form.error.exists"
+                            values={ values }/>
+                    );
+                }
+            }
 
             content = (
                 <form method="post"
                     className="SignUpSplash-form"
                     onSubmit={ this.onSubmit.bind(this) }>
                     <Msg tagName="h1" id="pages.landing.splash.form.h1"/>
+                    { errorMessage }
                     <input name="fn" placeholder={ msg('firstName') }/>
                     <input name="ln" placeholder={ msg('lastName') }/>
                     <input name="email" placeholder={ msg('email') }/>
@@ -55,14 +96,11 @@ export default class SignUpSplash extends React.Component {
     onSubmit(ev) {
         ev.preventDefault();
 
-        let data = {
-            first_name: ev.target.fn.value,
-            last_name: ev.target.ln.value,
-            email: ev.target.email.value,
-            password: ev.target.password.value,
-        };
-
-        // TODO: Submit to API
-        console.log(data);
+        this.props.dispatch(register(
+            ev.target.fn.value,
+            ev.target.ln.value,
+            ev.target.email.value,
+            ev.target.password.value,
+        ));
     }
 }
