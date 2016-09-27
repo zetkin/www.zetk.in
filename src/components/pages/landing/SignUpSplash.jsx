e1bdb5e6312fc61f114513067a7fbbccd1845c90
Richard Olsson <r@richardolsson.se>
2016-09-27 08:20:15 +0200

    Internationalize SignUpSplash and ZetkinIntro

--- src/components/pages/landing/SignUpSplash.jsx
+++ src/components/pages/landing/SignUpSplash.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { FormattedMessage as Msg } from 'react-intl';
 
 
 export default class SignUpSplash extends React.Component {
     render() {
         return (
             <div className="SignUpSplash">
-                <h1>Motivational headline</h1>
-                <h2>Very nice subheader</h2>
-                <Link to="/register">Ok, register!</Link>
-                <Link to="/info">Tell me more</Link>
+                <Msg tagName="h1" id="landing.splash.h1"/>
+                <Msg tagName="h2" id="landing.splash.h2"/>
+                <Link to="/register">
+                    <Msg id="landing.splash.signUpLink"/></Link>
+                <Link to="/info">
+                    <Msg id="landing.splash.infoLink"/></Link>
                 <div className="SignUpSplash-imageCaption">
-                    Do you want your photo here?
+                    <Msg tagName="p" id="landing.splash.caption"/>
                 </div>
             </div>
         );
