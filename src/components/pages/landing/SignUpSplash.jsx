9746c511eb34ee60b1ad3fca005afe802348aa52
Richard Olsson <r@richardolsson.se>
2016-09-27 09:39:00 +0200

    Change message path for SignUpSplash and ZetkinIntro

--- src/components/pages/landing/SignUpSplash.jsx
+++ src/components/pages/landing/SignUpSplash.jsx
@@ -7,14 +7,14 @@ export default class SignUpSplash extends React.Component {
     render() {
         return (
             <div className="SignUpSplash">
-                <Msg tagName="h1" id="landing.splash.h1"/>
-                <Msg tagName="h2" id="landing.splash.h2"/>
+                <Msg tagName="h1" id="pages.landing.splash.h1"/>
+                <Msg tagName="h2" id="pages.landing.splash.h2"/>
                 <Link to="/register">
-                    <Msg id="landing.splash.signUpLink"/></Link>
+                    <Msg id="pages.landing.splash.signUpLink"/></Link>
                 <Link to="/info">
-                    <Msg id="landing.splash.infoLink"/></Link>
+                    <Msg id="pages.landing.splash.infoLink"/></Link>
                 <div className="SignUpSplash-imageCaption">
-                    <Msg tagName="p" id="landing.splash.caption"/>
+                    <Msg tagName="p" id="pages.landing.splash.caption"/>
                 </div>
             </div>
         );
--- src/components/pages/landing/ZetkinIntro.jsx
+++ src/components/pages/landing/ZetkinIntro.jsx
@@ -6,13 +6,13 @@ export default class ZetkinIntro extends React.Component {
     render() {
         return (
             <div className="ZetkinIntro">
-                <Msg tagName="h2" id="landing.zetkin.h1"/>
-                <Msg tagName="h3" id="landing.zetkin.h2"/>
-                <Msg tagName="p" id="landing.zetkin.paragraph"/>
+                <Msg tagName="h2" id="pages.landing.zetkin.h1"/>
+                <Msg tagName="h3" id="pages.landing.zetkin.h2"/>
+                <Msg tagName="p" id="pages.landing.zetkin.paragraph"/>
                 <ul className="ZetkinIntro-highlights">
-                    <Msg tagName="li" id="landing.zetkin.usp1"/>
-                    <Msg tagName="li" id="landing.zetkin.usp2"/>
-                    <Msg tagName="li" id="landing.zetkin.usp3"/>
+                    <Msg tagName="li" id="pages.landing.zetkin.usp1"/>
+                    <Msg tagName="li" id="pages.landing.zetkin.usp2"/>
+                    <Msg tagName="li" id="pages.landing.zetkin.usp3"/>
                 </ul>
             </div>
         );
