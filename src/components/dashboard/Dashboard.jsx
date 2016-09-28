b569d90252559cad32b5d2fce4b5fe45a7b0a2a4
Richard Olsson <r@richardolsson.se>
2016-09-28 14:34:15 +0200

    Create CampaignList component, add to Dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage as Msg } from 'react-intl';
 
+import { retrieveAllCampaigns } from '../../actions/campaign';
 import { retrieveUserActions } from '../../actions/action';
 
 import ActionList from './ActionList';
+import CampaignList from './CampaignList';
 import PropTypes from '../../utils/PropTypes';
 
 
 const mapStateToProps = state => ({
+    campaignList: state.getIn(['campaigns', 'campaignList']),
     userActionList: state.getIn(['actions', 'userActionList']),
 });
 
@@ -22,15 +25,21 @@ export default class Dashboard extends React.Component {
         if (!this.props.userActionList.get('items')) {
             this.props.dispatch(retrieveUserActions());
         }
+
+        if (!this.props.campaignList.get('items')) {
+            this.props.dispatch(retrieveAllCampaigns());
+        }
     }
 
     render() {
         let actionList = this.props.userActionList;
+        let campaignList = this.props.campaignList;
 
         return (
             <div className="Dashboard">
                 <div className="Dashboard-campaigns">
                     <Msg tagName="h2" id="dashboard.campaigns.h"/>
+                    <CampaignList campaignList={ campaignList }/>
                 </div>
                 <div className="Dashboard-events">
                     <Msg tagName="h2" id="dashboard.events.h"/>
