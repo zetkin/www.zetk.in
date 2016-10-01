6ef90dbb738dd2dff058ab403c404a112bdc06d8
Richard Olsson <r@richardolsson.se>
2016-10-01 14:38:30 +0200

    Add action to update action response

--- src/actions/action.js
+++ src/actions/action.js
@@ -32,3 +32,28 @@ export function retrieveUserActions() {
         });
     };
 }
+
+export function updateActionResponse(action, responseBool) {
+    return ({ dispatch, getState, z }) => {
+        let orgId = action.get('org_id');
+        let actionId = action.get('id');
+
+        // Find user personId from list of memberships
+        let membershipList = getState().getIn(['orgs', 'membershipList']);
+        let membership = membershipList.get('items').find(val => (
+            val.getIn(['organization', 'id']) == orgId));
+        let personId = membership.getIn(['profile', 'id']);
+
+        let resource = z.resource('orgs', orgId, 'actions', actionId,
+                'responses', personId);
+
+        dispatch({
+            type: types.UPDATE_ACTION_RESPONSE,
+            meta: { actionId, responseBool },
+            payload: {
+                // PUT or DELETE depending on whether response is yes or no
+                promise: (responseBool? resource.put() : resource.del()),
+            }
+        });
+    };
+}
