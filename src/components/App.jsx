0326d38449868057dfd1647351e19c075c425ce6
Victor Wihlborg <jag@vbw.se>
2016-10-09 14:05:05 +0200

    Add fonts from typekit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,8 @@ export default class App extends React.Component {
         return (
             <html>
                 <head>
+                    <script src="https://use.typekit.net/tqq3ylv.js"></script>
+                    <script>{"try{Typekit.load({ async: true })}catch(e){}"}</script>
                     <title>Zetkin</title>
                     <script src="/static/main.js"></script>
                     <link rel="stylesheet" href="/static/css/style.css"/>
