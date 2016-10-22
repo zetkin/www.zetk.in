681d7d295bf2eeacb127900f91cc07219dc0b4c6
Victor Wihlborg <jag@vbw.se>
2016-10-22 21:23:44 +0200

    Style features section

--- src/components/pages/landing/ZetkinFeatures.jsx
+++ src/components/pages/landing/ZetkinFeatures.jsx
@@ -6,30 +6,42 @@ import { Link } from 'react-router';
 export default class ZetkinFeatures extends React.Component {
     render() {
         return (
-            <div className="ZetkinFeatures">
+            <section className="ZetkinFeatures">
                 <Msg id="pages.landing.features.h1" tagName="h2"/>
                 <Msg id="pages.landing.features.h2" tagName="h3"/>
                 <ul className="ZetkinFeatures-features">
-                    <li>
+                    <li className="ZetkinFeatures-feature">
+                        <img src="/static/img/logo-white.png"
+                            className="ZetkinFeatures-featureIcon"
+                            alt="Zetkin"/>
                         <Msg id="pages.landing.features.f1.title" tagName="h4"/>
                         <Msg id="pages.landing.features.f1.desc" tagName="p"/>
                     </li>
-                    <li>
+                    <li className="ZetkinFeatures-feature">
+                        <img src="/static/img/logo-white.png"
+                            className="ZetkinFeatures-featureIcon"
+                            alt="Zetkin"/>
                         <Msg id="pages.landing.features.f2.title" tagName="h4"/>
                         <Msg id="pages.landing.features.f2.desc" tagName="p"/>
                     </li>
-                    <li>
+                    <li className="ZetkinFeatures-feature">
+                        <img src="/static/img/logo-white.png"
+                            className="ZetkinFeatures-featureIcon"
+                            alt="Zetkin"/>
                         <Msg id="pages.landing.features.f3.title" tagName="h4"/>
                         <Msg id="pages.landing.features.f3.desc" tagName="p"/>
                     </li>
-                    <li>
+                    <li className="ZetkinFeatures-feature">
+                        <img src="/static/img/logo-white.png"
+                            className="ZetkinFeatures-featureIcon"
+                            alt="Zetkin"/>
                         <Msg id="pages.landing.features.f4.title" tagName="h4"/>
                         <Msg id="pages.landing.features.f4.desc" tagName="p"/>
                     </li>
                 </ul>
                 <Link to="/register">
                     <Msg id="pages.landing.features.signUpLink"/></Link>
-            </div>
+            </section>
         );
     }
 }
