599e9ded127ca50fcba34c70d7e401b387d13edb
Jens Börjesson <jens@dreamsinteractive.se>
2016-11-01 19:53:15 +0100

    ActionFormTime component

---
+++ src/components/campaign/action/ActionFormTime.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FormattedMessage as Msg } from 'react-intl';
+
+
+export default function ActionFormTime(props) {
+    return (
+        <div className="ActionFormTime">
+            { props.time }
+        </div>
+    );
+};
