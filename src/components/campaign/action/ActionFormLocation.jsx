48204a305e13b0f90ed293e9633f0ffff307c8f5
Jens Börjesson <jens@dreamsinteractive.se>
2016-11-01 19:52:24 +0100

    ActionFormLocation component

---
+++ src/components/campaign/action/ActionFormLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FormattedMessage as Msg } from 'react-intl';
+
+
+export default function ActionFormLocation(props) {
+    return (
+        <div className="ActionFormLocation">
+            { props.location }
+        </div>
+    );
+};
