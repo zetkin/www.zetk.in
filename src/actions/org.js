e929154f1eb55a716d9fe3ea47d89a8a837b8090
Richard Olsson <r@richardolsson.se>
2017-10-06 17:44:21 +0200

    Add action and store logic to retrieve single organization

--- src/actions/org.js
+++ src/actions/org.js
@@ -1,6 +1,17 @@
 import * as types from '.';
 
 
+export function retrieveOrganization(slugOrId) {
+    return ({ dispatch, z }) => {
+        dispatch({
+            type: types.RETRIEVE_ORGANIZATION,
+            payload: {
+                promise: z.resource('orgs', slugOrId).get(),
+            },
+        });
+    };
+}
+
 export function retrieveUserMemberships() {
     return ({ dispatch, z }) => {
         dispatch({
