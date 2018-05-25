af32ca18cfdd3f1e49c1088b8f5c1e561855d963
Victor Baquero Wihlborg <jag@vbw.se>
2018-05-25 15:05:07 +0200

    Add SignUpForm to landing page

--- src/components/pages/landing/SignUpSplash.jsx
+++ src/components/pages/landing/SignUpSplash.jsx
@@ -3,12 +3,10 @@ import cx from 'classnames';
 import { FormattedMessage as Msg, injectIntl } from 'react-intl';
 import React from 'react';
 
-import { register } from '../../../actions/register';
 import FormattedLink from '../../../common/misc/FormattedLink';
-
+import SignUpForm from '../../misc/SignUpForm';
 
 const mapStateToProps = state => ({
-    register: state.get('register'),
     user: state.get('user'),
 });
 
@@ -16,27 +14,13 @@ const mapStateToProps = state => ({
 @injectIntl
 export default class SignUpSplash extends React.Component {
     static propTypes = {
-        showForm: React.PropTypes.bool,
-        onSignUp: React.PropTypes.func,
+        showForm: React.PropTypes.bool
     };
 
     render() {
-        let store = this.props.register;
         let content;
 
-        if (this.props.showForm && store.get('isComplete')) {
-            let values = store.get('data').toJS();
-
-            content = (
-                <div className="SignUpSplash-done">
-                    <Msg tagName="h1" id="pages.landing.splash.done.h1"
-                        values={ values }/>
-                    <Msg tagName="h2" id="pages.landing.splash.done.h2"
-                        values={ values }/>
-                </div>
-            );
-        }
-        else if (this.props.showForm && this.props.user.get('data')) {
+        if (this.props.showForm && this.props.user.get('data')) {
             let domain = process.env.ZETKIN_DOMAIN;
             let userData = this.props.user.get('data');
             let firstName = userData.get('first_name');
@@ -67,49 +51,7 @@ export default class SignUpSplash extends React.Component {
             );
         }
         else if (this.props.showForm) {
-            let msg = (id, values) => this.props.intl.formatMessage({
-                id: 'pages.landing.splash.form.' + id }, values);
-
-            let error = store.get('error');
-            let errorMessage;
-            if (error) {
-                if (error.httpStatus == 409) {
-                    let values = store.get('data').toJS();
-
-                    errorMessage = (
-                        <div className="SignUpSplash-error">
-                            <Msg id="pages.landing.splash.form.error.exists"
-                            values={ values }/>
-                        </div>
-                    );
-                }
-                else if (error.httpStatus == 400) {
-                    errorMessage = (
-                        <div className="SignUpSplash-error">
-                            <Msg id="pages.landing.splash.form.error.invalid"/>
-                        </div>
-                    );
-                }
-            }
-
-            content = (
-                <form method="post"
-                    className="SignUpSplash-form"
-                    onSubmit={ this.onSubmit.bind(this) }>
-                    <Msg tagName="h1" id="pages.landing.splash.form.h1"/>
-                    { errorMessage }
-                    <label htmlFor="fn">{ msg('firstName') }</label>
-                    <input name="fn" placeholder={ msg('firstName') }/>
-                    <label htmlFor="ln">{ msg('lastName') }</label>
-                    <input name="ln" placeholder={ msg('lastName') }/>
-                    <label htmlFor="email">{ msg('email') }</label>
-                    <input name="email" placeholder={ msg('email') }/>
-                    <label htmlFor="password">{ msg('password') }</label>
-                    <input type="password" name="password"
-                        placeholder={ msg('password') }/>
-                    <input type="submit" value={ msg('submitButton') }/>
-                </form>
-            );
+            content = <SignUpForm/>;
         }
         else {
             let infoHref = this.props.intl.formatMessage(
@@ -141,16 +83,5 @@ export default class SignUpSplash extends React.Component {
                 </div>
             </div>
         );
-    }
-
-    onSubmit(ev) {
-        ev.preventDefault();
-
-        this.props.dispatch(register(
-            ev.target.fn.value,
-            ev.target.ln.value,
-            ev.target.email.value,
-            ev.target.password.value,
-        ));
     }
 }
